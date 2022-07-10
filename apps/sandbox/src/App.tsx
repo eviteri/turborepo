aeec159b763ac041c04b20aeca9847dc674da392
Eloy Viteri <eviterif@gmail.com>
2022-07-10 13:37:13 -0400

    added a new package

--- apps/sandbox/src/App.tsx
+++ apps/sandbox/src/App.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import {Button} from '@eviteri/turborepo-ui'
+import {Button as StoreButton} from '@eviteri/turborepo-store'
 
 const App: React.FC = () => {
-  return (<div> <Button /></div>)
+  return (<div> <Button /><StoreButton /></div>)
 }
 
 export default App;
